96fef51ae6296d41abf698cd285e2c4f1dd550cc
David Liou <david.liou@nih.gov>
2022-02-24 18:51:58 -0500

    Trim benchmark results based on standard deviation

--- src/main/java/net/noisynarwhal/albrecht/benchmark/Benchmark.java
+++ src/main/java/net/noisynarwhal/albrecht/benchmark/Benchmark.java
@@ -6,23 +6,31 @@
 package net.noisynarwhal.albrecht.benchmark;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import net.noisynarwhal.albrecht.Population;
 
 /**
+ * Benchmark evolution times
  *
  * @author lioudt
  */
 public class Benchmark {
 
+    /**
+     * Run a number of evolutions and calculate average number of evolution
+     * times in units of successes per second.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         final int order = 9;
         final int populationSize = 200;
         final int numTrials = 200;
-        
+
         System.gc();
 
         final List<Long> times = new ArrayList<>();
@@ -42,9 +50,19 @@ public class Benchmark {
 
         Collections.sort(times);
 
-        final int trim = Math.round(times.size() / 20.0F);
-        for (int i = 0; i < trim && !times.isEmpty(); i++) {
+        final double mean = Benchmark.mean(times);
+        final double sd = Benchmark.sd(times);
+
+        /**
+         * Trim the results to remove outliers
+         */
+        final double headCutOff = mean - (2.5 * sd);
+        final double tailCutOff = mean + (2.5 * sd);
+
+        while (times.get(0) < headCutOff) {
             times.remove(0);
+        }
+        while (times.get(times.size() - 1) > tailCutOff) {
             times.remove(times.size() - 1);
         }
 
@@ -56,6 +74,48 @@ public class Benchmark {
 
         final double average = ((double) times.size()) / ((double) secs);
         System.out.println("Adjusted average: " + average);
+    }
+
+    /**
+     * Calculate standard deviation
+     *
+     * @param values
+     * @return
+     */
+    private static double sd(Collection<Long> values) {
+
+        final int len = values.size();
+
+        double sum = 0.0;
+        for (final double v : values) {
+            sum += v;
+        }
+
+        final double mean = sum / len;
+
+        double sd = 0.0;
+        for (final double v : values) {
+            sd += Math.pow(v - mean, 2);
+        }
+
+        return Math.sqrt(sd / len);
+    }
+
+    /**
+     * Return the average
+     *
+     * @param values
+     * @return
+     */
+    private static double mean(Collection<Long> values) {
+
+        double sum = 0.0;
+
+        for (final double v : values) {
+            sum += v;
+        }
+
+        return sum / values.size();
     }
 
 }
